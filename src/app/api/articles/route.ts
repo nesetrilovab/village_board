c06687f1447dd4b77618f6476d1e83c111123938
nesetrilovab <144663108+nesetrilovab@users.noreply.github.com>
2026-02-25 22:18:31 +0100

    ui, post done

---
+++ src/app/api/articles/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+
+    const article = await prisma.articles.create({
+      data: body,
+    });
+
+    return NextResponse.json(article);
+  } catch (error) {
+    console.error("API ERROR:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
